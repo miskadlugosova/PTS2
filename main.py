ad35ea0690db7db63b4905e829d5557c19d1660c
miskadlugosova <miskadlugosova@gmail.com>
2020-04-11 13:37:11 +0200

    Starting from the beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from itertools import count
-from messages import *
 
 
 class Reservation(object):
@@ -12,41 +11,42 @@ class Reservation(object):
         self._book = book
         self._for = for_
         self._changes = 0
-        res_init_message(self._id, self._book, self._from, self._to, self._for)
+        print(F'Created a reservation with id {self._id} of {self._book} ' +
+              F'from {self._from} to {self._to} for {self._for}.')
 
     def overlapping(self, other):
         ret = (self._book == other._book and self._to >= other._from
                and self._to >= other._from)
+        str = 'do'
         if not ret:
-            res_overlapping_message(self._id, other._id, 'do not')
-        else:
-            res_overlapping_message(self._id, other._id, 'do')
+            str = 'do not'
+        print(F'Reservations {self._id} and {other._id} {str} overlap')
         return ret
 
     def includes(self, date):
         ret = (self._from <= date <= self._to)
+        str = 'includes'
         if not ret:
-            res_includes_message(self._id, 'does not include', date)
-        else:
-            res_includes_message(self._id, 'includes', date)
-
+            str = 'does not include'
+        print(F'Reservation {self._id} {str} {date}')
         return ret
 
     def identify(self, date, book, for_):
         if book != self._book:
-            res_identity_message('book', book, for_, date, self)
+            print(F'Reservation {self._id} reserves {self._book} not {book}.')
             return False
         if for_ != self._for:
-            res_identity_message('for', book, for_, date, self)
+            print(F'Reservation {self._id} is for {self._for} not {for_}.')
             return False
         if not self.includes(date):
-            res_identity_message('includes', book, for_, date, self)
+            print(F'Reservation {self._id} is from {self._from} to {self._to} which ' +
+                  F'does not include {date}.')
             return False
-        res_identity_message('ok', book, for_, date, self)
+        print(F'Reservation {self._id} is valid {for_} of {book} on {date}.')
         return True
 
     def change_for(self, for_):
-        res_change_for_message(self._id, self._for, for_)
+        print(F'Reservation {self._id} moved from {self._for} to {for_}')
         self._for = for_
 
 
@@ -55,30 +55,33 @@ class Library(object):
         self._users = set()
         self._books = {}  # maps name to count
         self._reservations = []  # Reservations sorted by from
-        lib_init_message()
+        print(F'Library created.')
 
     def add_user(self, name):
         if name in self._users:
-            lib_add_user_message(False, name)
+            print(F'User not created, user with name {name} already exists.')
             return False
         self._users.add(name)
-        lib_add_user_message(True, name)
+        print(F'User {name} created.')
         return True
 
     def add_book(self, name):
         self._books[name] = self._books.get(name, 0) + 1
-        lib_add_book_message(name, self._books[name])
+        print(F'Book {name} added. We have {self._books[name]} coppies of the book.')
 
     def reserve_book(self, user, book, date_from, date_to):
         book_count = self._books.get(book, 0)
         if user not in self._users:
-            lib_reserve_book_message('user', book, user, date_from, date_to)
+            print(F'We cannot reserve book {book} for {user} from {date_from} to {date_to}. ' +
+                  F'User does not exist.')
             return False
         if date_from > date_to:
-            lib_reserve_book_message('date', book, user, date_from, date_to)
+            print(F'We cannot reserve book {book} for {user} from {date_from} to {date_to}. ' +
+                  F'Incorrect dates.')
             return False
         if book_count == 0:
-            lib_reserve_book_message('count', book, user, date_from, date_to)
+            print(F'We cannot reserve book {book} for {user} from {date_from} to {date_to}. ' +
+                  F'We do not have that book.')
             return False
         desired_reservation = Reservation(date_from, date_to, book, user)
         relevant_reservations = [res for res in self._reservations
@@ -88,32 +91,32 @@ class Library(object):
         for from_ in [res._from for res in relevant_reservations]:
             if desired_reservation.includes(from_):
                 if sum([rec.includes(from_) for rec in relevant_reservations]) > book_count:
-                    lib_reserve_book_message('not_enough', book, user, date_from, date_to)
+                    print(F'We cannot reserve book {book} for {user} from {date_from} ' +
+                          F'to {date_to}. We do not have enough books.')
                     return False
         self._reservations += [desired_reservation]
         self._reservations.sort(key=lambda x: x._from)  # to lazy to make a getter
-        lib_reserve_book_message('ok', book, user, date_from, date_to, desired_reservation._id)
-        ##!nechcem posielat vsetky zvysne argumenty!
+        print(F'Reservation {desired_reservation._id} included.')
         return True
 
     def check_reservation(self, user, book, date):
         res = any([res.identify(date, book, user) for res in self._reservations])
+        str = 'exists'
         if not res:
-            lib_check_reservation_message(user, book, date, 'does not exist')
-        else:
-            lib_check_reservation_message(user, book, date, 'exists')
+            str = 'does not exist'
+        print(F'Reservation for {user} of {book} on {date} {str}.')
         return res
 
     def change_reservation(self, user, book, date, new_user):
         relevant_reservations = [res for res in self._reservations
                                  if res.identify(date, book, user)]
         if not relevant_reservations:
-            lib_change_reservation('reservations', user, book, date, new_user)
+            print(F'Reservation for {user} of {book} on {date} does not exist.')
             return False
         if new_user not in self._users:
-            lib_change_reservation('user', user, book, date, new_user)
+            print(F'Cannot change the reservation as {new_user} does not exist.')
             return False
 
-        lib_change_reservation('ok', user, book, date, new_user)
+        print(F'Reservation for {user} of {book} on {date} changed to {new_user}.')
         relevant_reservations[0].change_for(new_user)
         return True
