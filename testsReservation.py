2c048c8d6977b7106a92f6126e5042b9efa979c6
miskadlugosova <miskadlugosova@gmail.com>
2020-04-15 21:55:50 +0200

    added missing cases in overlapping, replaced assertEqual(..., True/False) with assertTrue and assertFalse, testing change_for if really changes

--- testsReservation.py
+++ testsReservation.py
@@ -1,32 +1,37 @@
 import unittest
 from main import TemplateReservation
 
+
 class Test_Template_Reservation(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.reserve1 = TemplateReservation(10, 24, 'Traja patraci', 'Miska')
         self.reserve2 = TemplateReservation(24, 26, 'Traja patraci', 'Adam')
         self.reserve3 = TemplateReservation(1, 6, 'Traja patraci', 'Katka')
-        self.reserve4 = TemplateReservation(10, 15, 'Sherlock Holmes', 'Katka')
+        self.reserve4 = TemplateReservation(11, 15, 'Sherlock Holmes', 'Katka')
+        self.reserve5 = TemplateReservation(11, 15, 'Traja patraci', 'Katka')
+        self.reserve6 = TemplateReservation(7, 14, 'Traja patraci', 'Jano')
 
-#maybe test also from1 < from2 < to1 < to2 and from2 < from1 <to2 <to1
     def test_overlapping(self):
-        self.assertEqual(self.reserve1.overlapping(self.reserve2), True)  # last day
-        self.assertEqual(self.reserve2.overlapping(self.reserve1), True)  # first day
-        self.assertEqual(self.reserve1.overlapping(self.reserve3), False)  # sooner
-        self.assertEqual(self.reserve3.overlapping(self.reserve1), False)  # later
-        self.assertEqual(self.reserve1.overlapping(self.reserve4), False)  # middle
-        self.assertEqual(self.reserve4.overlapping(self.reserve1), False)  # around
+        self.assertTrue(self.reserve1.overlapping(self.reserve2))  # last day
+        self.assertTrue(self.reserve2.overlapping(self.reserve1))  # first day
+        self.assertTrue(self.reserve1.overlapping(self.reserve6))   # cross
+        self.assertTrue(self.reserve1.overlapping(self.reserve5))   # middle
+        self.assertTrue(self.reserve5.overlapping(self.reserve1))   # around
+        self.assertTrue(self.reserve1.overlapping(self.reserve1))   # same
+        self.assertFalse(self.reserve1.overlapping(self.reserve3))  # sooner
+        self.assertFalse(self.reserve3.overlapping(self.reserve1))  # later
+        self.assertFalse(self.reserve1.overlapping(self.reserve4))  # different book
 
     def test_includes(self):
-        self.assertEqual(self.reserve1.includes(4), False)  # not, before
-        self.assertEqual(self.reserve1.includes(10), True)  # first day
-        self.assertEqual(self.reserve1.includes(15), True)  # middle
-        self.assertEqual(self.reserve1.includes(24), True)  # last day
-        self.assertEqual(self.reserve1.includes(25), False)  # not, after
+        self.assertFalse(self.reserve1.includes(4))  # before
+        self.assertTrue(self.reserve1.includes(10))  # first day
+        self.assertTrue(self.reserve1.includes(15))  # middle
+        self.assertTrue(self.reserve1.includes(24))  # last day
+        self.assertFalse(self.reserve1.includes(25))  # after
 
-    #you should check if the TemplateReservation._for changes properly: self.assertEqual(self.res_1._for, 'Helen') - after that you don 't have to test test_identify_change_identify
     def test_change_for(self):
         self.assertEqual(self.reserve1.change_for('Jano'), None)
+        self.assertEqual(self.reserve1._for, 'Jano')
 
     def test_identify(self):
         self.assertEqual(self.reserve3.identify(5, 'Traja patraci', 'Katka'), (True, 'ok'))
@@ -35,10 +40,10 @@ class Test_Template_Reservation(unittest.TestCase):
         self.assertEqual(self.reserve3.identify(4, 'Poirot', 'Katka'), (False, 'book'))
         self.assertEqual(self.reserve3.identify(2, 'Traja patraci', 'Adam'), (False, 'for'))
 
-    def test_identify_change_identify(self):  # change was successful
-        self.assertEqual(self.reserve1.identify(20, 'Traja patraci', 'Miska'), (True, 'ok'))
-        self.assertEqual(self.reserve1.change_for('Jano'), None)
-        self.assertEqual(self.reserve1.identify(20, 'Traja patraci', 'Jano'), (True, 'ok'))
+    #def test_identify_change_identify(self):  # change was successful                          #replaced by testing change_for by itself
+    #    self.assertEqual(self.reserve1.identify(20, 'Traja patraci', 'Miska'), (True, 'ok'))
+    #    self.assertEqual(self.reserve1.change_for('Jano'), None)
+    #    self.assertEqual(self.reserve1.identify(20, 'Traja patraci', 'Jano'), (True, 'ok'))
 
 if __name__ == '__main__':
     unittest.main()
